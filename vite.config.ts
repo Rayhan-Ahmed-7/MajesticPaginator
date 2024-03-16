5b8f547ecdd76bb117027b2ef08141f3765961a9
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2024-03-17 00:03:08 +0600

    :zap: css added

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
 import dts from "vite-plugin-dts"
+import css from "vite-plugin-css"
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
@@ -22,5 +23,8 @@ export default defineConfig({
     sourcemap: true,
     emptyOutDir: true,
   },
-  plugins: [react(), dts()],
+  plugins: [react(), dts(),css({
+    name:"majestic-paginator",
+    tranfrom:() =>  `style.css`
+  })],
 })
